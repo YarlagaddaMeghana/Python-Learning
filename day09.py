b6be2cf56b920143eaf29542c6a22bbc443b6d2f
YarlagaddaMeghana <meghanayarlagadda785@gmail.com>
2026-09-15 08:52:34 +0530

    day09

--- day09.py
+++ day09.py
@@ -1,31 +1,33 @@
 n=int(input())
+# right triangle
 for i in range(1,n+1):
     print(i*'*')
 
 print()
 
-# n=int(input())
+# inverted right triangle
 for i in range(n,0,-1):
     print(i*'*')
 
 print()
 
-# n=int(input())
+# triangle
 for i in range(1,n+1):
     print((n-i)*' ' + i*'* ')
 
 print()
-# n=int(input())
+# inverted triangle
 for i in range(n,0,-1):
     print((n-i)*' '+i*'* ')
 
 print()
-# n=int(input())
+# diamond
 for i in range(1,n+1):
     print((n-i)*' ' + i*'* ')
 for i in range(n-1,0,-1):
     print((n-i)*' '+i*'* ')
 print()
+# hollow square
 for i in range(n):
     for j in range(n):
         if i==0 or j==0 or i==n-1 or j==n-1:
@@ -53,7 +55,7 @@ for i in range(n):
         print((i+1)*'*'+(n+1-2*i+1)*' '+(i+1)*'*',end=' ')
     print()
 print()
-
+# star
 for i in range(n):
     for j in range(n):
         if i==j or j==n-i-1 or i==n//2 or j==n//2:
